a411213889652b9304577871781b15a6b914e27a
Ranger-HK <maxravindu99@gmail.com>
2022-07-30 00:34:41 +0530

    'customerService'

--- fontend/src/services/CustomerService.js
+++ fontend/src/services/CustomerService.js
@@ -1,19 +1,52 @@
-import axios from "../axios";
-import qs from "qs";
+import axios from '../axios';
+import qs from 'qs';
 
-class CustomerService{
-    postCustomer=async (data) => {
+class CustomerService {
+    postCustomer = async (data) => {
         const promise = new Promise((resolve, reject) => {
-            axios.post("customer", qs.stringify(data))
+            axios.post('customer', qs.stringify(data))
                 .then((res) => {
-                    return resolve(res);
+                    return resolve(res)
                 }).catch((err) => {
-                return resolve(err);
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+
+    fetchCustomer = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get('customer')
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+    deleteCustomers = async (params) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.delete('customer',{params:params})
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
             })
         })
         return await promise;
     }
-
+    customerCount = async () => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get('customer/count')
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        })
+        return await promise;
+    }
 }
-export default new CustomerService();
 
+export default new CustomerService();
